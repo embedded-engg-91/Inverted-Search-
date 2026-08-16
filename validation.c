fa1a054237c8f29a2b4ea1a612fd2f21d9fd8491
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-16 11:51:33 +0530

    Update validation.c
    Refactor validation checks for file input in read_validate function.

--- validation.c
+++ validation.c
@@ -14,22 +14,17 @@ FILE *exist_and_non_empty(char *argv)
 
 Status read_validate(char **argv, Slist **head)
 {
-    /*Validations
-    1. The File is ".txt"
-    2. The File is Existing
-    3. The File isnt Empty
-    4. The File isnt repaeted in the List of file(SLL)
-    */
+    
     int count = 0;
     for (int i = 0; argv[i] != NULL; i++)
     {
-        // toknow the file is txt we do strrchr then strcmp
+        
         char *temp = NULL;
-        if ((temp = strrchr(argv[i], '.')) != NULL) // means dot i s presernt
+        if ((temp = strrchr(argv[i], '.')) != NULL) 
         {
-            if ((strcmp(temp, ".txt")) == 0) // means .txt is at the end
+            if ((strcmp(temp, ".txt")) == 0) 
             {
-                // now the file name has .txt means the file is valid
+                
                 FILE *fp;
                 if ((fp = exist_and_non_empty(argv[i])) != NULL &&
                     find_node(*head, argv[i]) == SUCCESS)
